b2ea03fcbd25e3925e82e51eee24920d1cf95067
Emile Arseneault <emile.arseneault@usherbrooke.ca>
2019-03-04 22:25:21 -0500

    Copied help message from original morning

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,21 @@ fn open_config(){
 }
 
 fn print_help(){
-    println!("Morning program");
-    println!("usage : morning");
+    println!();
+    println!("Usage : morning [OPTION]...");
+	println!("----------------------------");
+	println!();
+	println!("  -n                 Used to write a message to print to tomorrow.");
+	println!("                       Can be used with a positive integer to");
+	println!("                       report the message by the number of days.");
+	println!("  -w                 Used as a shortcut to 'morning -n 3' to be used");
+	println!("                       report the message by the number of days.");
+	println!("  -p                 Display past messages by the number of days given.");
+	println!("                       Messages have to be in history time interval.");
+	println!("                       (config variable historytime >= days given)");
+	println!("  -r                 Open reminders file in user's default editor.");
+	println!("  -c                 Open commands file in user's default editor.");
+	println!();
+    println!("  -h,--help          Display this help and exit.");
+    println!();
 }
